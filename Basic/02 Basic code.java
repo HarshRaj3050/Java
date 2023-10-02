2f0b53fe464a2b511a53561c634327a8d4119629
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-02 16:08:28 +0530

    update

---
+++ Basic/02 Basic code.java
@@ -0,0 +1,11 @@
+class Mainn
+{
+    public static void main(String[] args)
+    {
+        int num1 = 5;
+        int num2 = 8;
+        int num3 = 2;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
