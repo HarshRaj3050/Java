59c624d31b26b2aea321691bf07e3dea868a2664
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-03 17:56:14 +0530

    practice

---
+++ Basic/Basic_Practice.java
@@ -0,0 +1,25 @@
+//  Write a program to calculate the percentage of a given student in the CBSE board exam.
+//  His marks from 5 subjects must be taken as input from the keyboard.
+
+import java.util.Scanner;
+public class Basic_Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("-----Enter All the subject marks ( out of 100 )-----");
+        System.out.print("Enter First subject marks : ");
+        float sub1 = scan.nextInt();
+        System.out.print("Enter Second subject marks : ");
+        int sub2 = scan.nextInt();
+        System.out.print("Enter Third subject marks : ");
+        int sub3 = scan.nextInt();
+        System.out.print("Enter Fourth subject marks : ");
+        int sub4 = scan.nextInt();
+        System.out.print("Enter Fifth subject marks : ");
+        int sub5 = scan.nextInt();
+
+        float average = ((sub1 + sub2 + sub3 + sub4 + sub5) * 100) / 500;
+        System.out.print("The average marks is : ");
+        System.out.println(average);
+
+    }
+}
