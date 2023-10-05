4336fd566bc36a15dd552c488a3814b6a3ff755f
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-05 20:25:34 +0530

    new question

---
+++ JAVA/Java_03_Strings/Strings_01_Code.java
@@ -0,0 +1,10 @@
+package Java_03_Strings;
+
+public class Strings_01_Code {
+    public static void main(String[] args) {
+        String a = "Harsh";
+        String b = "Harsh";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
