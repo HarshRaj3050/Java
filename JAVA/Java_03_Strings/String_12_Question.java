b85452e457951f618ed64bbbdd4b8944e61f334a
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-06 18:26:33 +0530

    new question

--- JAVA/Java_03_Strings/String_12_Question.java
+++ JAVA/Java_03_Strings/String_12_Question.java
@@ -1,4 +1,18 @@
-package Java_03_Strings;
+/* Write a Java program to fill in a letter template which looks like below:
 
+    '''  letter = “Dear <|name|>, Thanks a lot” '''
+         Replace <|name|> with a string (some name)
+ */
+
+package Java_03_Strings;
+import java.util.Scanner;
 public class String_12_Question {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String letter = "letter = “Dear <|name|>, Thanks a lot”";
+        System.out.print("Enter Your Name : ");
+        String name = scan.nextLine();
+        System.out.println(letter.replace("<|name|>", name));
+    }
 }
