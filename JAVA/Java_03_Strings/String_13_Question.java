151c6d94fd4d7aa3af31c05a9175c91ccbc693f6
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-06 18:31:34 +0530

    new question

---
+++ JAVA/Java_03_Strings/String_13_Question.java
@@ -0,0 +1,12 @@
+// Write a program to format the following letter using escape sequence characters.
+    //  Letter = “Dear Harsh, This Java Course is nice. Thanks”
+
+package Java_03_Strings;
+public class String_13_Question {
+    public static void main(String[] args) {
+        System.out.println("-----Before Format-----");
+        System.out.println("Dear Harsh, This Java Course is nice. Thanks");
+        System.out.println("-----After Format using escape sequence characters-----");
+        System.out.println("Dear Harsh,\nThis Java Course is nice.\nThanks");
+    }
+}
