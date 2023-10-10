005c752768edd5c14655e5f99bd9758759ff61de
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-10 15:15:51 +0530

    new question

--- JAVA/Java_05_Loops/Loops_13_Question.java
+++ JAVA/Java_05_Loops/Loops_13_Question.java
@@ -1,4 +1,19 @@
-package Java_05_Loops;
+// Write a program to find the factorial of a given number using for loops.
 
+package Java_05_Loops;
+import java.util.Scanner;
 public class Loops_13_Question {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int num = scan.nextInt();
+        int i = 1;
+        int fact = 1;
+        while(i<=num) {
+            fact = fact * i;
+            i++;
+        }
+        System.out.printf("The Factorial of %d is : %d", num, fact);
+
+    }
 }
