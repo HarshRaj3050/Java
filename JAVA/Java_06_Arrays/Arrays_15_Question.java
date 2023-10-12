0bc5daf35583bbea73b8cf529f94e52a881e4b88
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-12 23:57:35 +0530

    new question

--- JAVA/Java_06_Arrays/Arrays_15_Question.java
+++ JAVA/Java_06_Arrays/Arrays_15_Question.java
@@ -2,5 +2,14 @@
 
 package Java_06_Arrays;
 public class Arrays_15_Question {
-    
+    public static void main(String[] args) {
+        int [] array = {34, 46, 23, 65, 13, 54};
+        int max = Integer.MIN_VALUE;
+        for (int i : array) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        System.out.println("Maximum element in an array is : " + max);
+    }
 }
---
+++ JAVA/Java_06_Arrays/Java_16_Question.java
@@ -0,0 +1,34 @@
+
+
+package Java_06_Arrays;
+import java.util.Scanner;
+public class Java_16_Question {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("---Enter Five Number (try to Enter Number in sorted form---");
+        int [] array = new int[5];
+        // User can Enter 5 Integer value
+        for(int i=0; i<array.length; i++) {
+            System.out.print("Enter the Number : ");
+            array[i] = scan.nextInt();
+        }
+
+        int prevArray = Integer.MIN_VALUE;
+        boolean check = true;
+        // Check the array One-by-One (Left-to-Right)
+        for (int i : array) {
+            if (prevArray > i) {
+                check = false;
+                break;
+            }
+            prevArray = i;
+        }
+
+        if(check) {
+            System.out.println("Array is Sorted");
+        }
+        else {
+            System.out.println("Array is Not Sorted");
+        }
+    }
+}
