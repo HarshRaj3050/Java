16be1f4daf7066e6f35214d7b90fc85a672ea1cf
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-13 23:32:42 +0530

    update

--- JAVA/Java_07_Method/Method_01_Code.java
+++ JAVA/Java_07_Method/Method_01_Code.java
@@ -1,4 +1,23 @@
 package Java_07_Method;
 
 public class Method_01_Code {
+    static class calculate{
+        int multiply(int a,int b){
+            return a*b;
+        }
+        double multiply(double a,double b){
+            return  a*b;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        calculate obj = new calculate();
+        int c = obj.multiply(5,4);
+        double d = obj.multiply(5.1,4.2);
+        System.out.println("Mutiply method : returns integer : " + c);
+        System.out.println("Mutiply method : returns double : " +  d);
+
+    }
+
 }
