9121f0e6d1925a20a08825c40db1d0ec2e77f0f4
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-18 23:32:31 +0530

    practice

---
+++ JAVA/Java_09_AccessModifiers_Constructors/Practice.java
@@ -0,0 +1,30 @@
+package Java_09_AccessModifiers_Constructors;
+import java.util.Scanner;
+class Emp{
+    private String name;
+    private int id;
+    void nameSet(String n) {
+        name = n;
+    }
+    String nameGet() {
+        return name;
+    }
+    void idSet(int i) {
+        id = i;
+    }
+    int idGet() {
+        return id;
+    }
+    void details() {
+        System.out.println("Empolyee Name is : " + nameGet() + "\nEmpolyee Id is : " + idGet());
+    }
+}
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Emp emp1 = new Emp();
+        emp1.nameSet("Harsh Raj");
+        emp1.idSet(1004);
+        emp1.details();
+    }
+}
