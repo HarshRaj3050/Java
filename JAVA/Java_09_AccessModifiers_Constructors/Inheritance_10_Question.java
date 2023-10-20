e985b898b3543fe26569a0edcc00aaf10d0457ba
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-20 23:57:36 +0530

    new question

--- JAVA/Java_09_AccessModifiers_Constructors/Inheritance_10_Question.java
+++ JAVA/Java_09_AccessModifiers_Constructors/Inheritance_10_Question.java
@@ -1,6 +1,29 @@
 // Create a class Animal and Derive another class dog from it
 
 package Java_09_AccessModifiers_Constructors;
-
+import java.util.Scanner;
+class Animals{
+    String animalName;
+    int animalNumber;
+    Animals(String animal, int animalNumber) {
+        this.animalName = animal;
+        this.animalNumber = animalNumber;
+    }
+}
+class Dog extends Animals {
+    Dog(String name, int number) {
+        super(name, number);
+        System.out.println("The Dog Name is : " + animalName);
+        System.out.println("Number of " + animalName + " is : " + animalNumber);
+    }
+}
 public class Inheritance_10_Question {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Dog Name : ");
+        String aName = scan.next();
+        System.out.print("Number of " + aName + " is : ");
+        int aNumber = scan.nextInt();
+        Animals dog = new Dog(aName, aNumber);
+    }
 }
