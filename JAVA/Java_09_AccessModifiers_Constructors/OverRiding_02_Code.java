bbd1ac56023cf9fde8883b69a401ce74afb3b435
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-22 22:30:55 +0530

    new question

---
+++ JAVA/Java_09_AccessModifiers_Constructors/OverRiding_02_Code.java
@@ -0,0 +1,29 @@
+package Java_09_AccessModifiers_Constructors;
+
+class Super {
+    int x;
+    Super(int x) {
+        this.x = x;
+    }
+    void display() {
+        System.out.println("Super x = " + x);
+        }
+    }
+class Sub extends Super {
+    int y;
+    Sub(int x, int y) {
+        super(x);
+        this.y = y;
+    }
+    @Override
+    void display() {
+        System.out.println("Super x = " + x);
+        System.out.println("Sub y = " + y);
+    }
+}
+public class OverRiding_02_Code {
+    public static void main(String[] args) {
+        Sub s1 = new Sub(100,200);
+        s1.display();
+    }
+}
