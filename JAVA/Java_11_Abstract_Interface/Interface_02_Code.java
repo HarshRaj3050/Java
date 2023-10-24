12c3424b4972161676ee8b2bb3f86267a1da63dd
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-24 22:44:40 +0530

    new question

--- JAVA/Java_11_Abstract_Interface/Interface_02_Code.java
+++ JAVA/Java_11_Abstract_Interface/Interface_02_Code.java
@@ -1,2 +1,24 @@
 // Example of java interface which provides the implementation of Bank interface.
 
+package Java_11_Abstract_Interface;
+interface Bank{
+    float rateOfInterest();
+}
+class SBI implements Bank{
+    @Override
+    public float rateOfInterest() {
+        return 9.15f;
+    }
+}
+class PNB implements Bank{
+    @Override
+    public float rateOfInterest() {
+        return 9.7f;
+    }
+}
+public class Interface_02_Code {
+    public static void main(String[] args) {
+        Bank b = new SBI();
+        System.out.println("ROI : " + b.rateOfInterest());
+    }
+}
