ccf4a8dedebf1a915393adf4bce3a3799d0f583c
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-26 23:01:37 +0530

    Practice

--- JAVA/Java_11_Abstract_Interface/Practice.java
+++ JAVA/Java_11_Abstract_Interface/Practice.java
@@ -1,4 +1,31 @@
-package Java_11_Abstract_Interface;
+/*   Create a class monkey with jump ( ) and bite ( ) methods Create a class human which inherits
+     this monkey class and implements BasicAnimal interface with eat ( ) and sleep ( ) methods
+ */
 
+package Java_11_Abstract_Interface;
+class Monkey1 {
+    void jump() {
+        System.out.println("Jumping");
+    }
+    void bite() {
+        System.out.println("Biting");
+    }
+}
+interface BasicAnimal1 {
+    void eat();
+    void sleep();
+}
+class Human1 extends Monkey1 implements BasicAnimal {
+    public void eat() {
+        System.out.println("Eating");
+    }
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+}
 public class Practice {
+    public static void main(String[] args) {
+        Human1 human = new Human1();
+        human.eat();
+    }
 }
