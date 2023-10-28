02f4c0b0854e3ff4c00a8331cf1aa85cbf6a116e
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-28 22:20:39 +0530

    Practice

--- JAVA/Java_10_Inheritance_OverRiding/Practice.java
+++ JAVA/Java_10_Inheritance_OverRiding/Practice.java
@@ -5,9 +5,34 @@
 */
 
 package Java_10_Inheritance_OverRiding;
-
+interface Bank1 {
+    int rateOfInterest();
+}
+class SBI1 implements Bank1 {
+    @Override
+    public int rateOfInterest() {
+        return 8;
+    }
+}
+class ICICI1 implements Bank1 {
+    @Override
+    public int rateOfInterest() {
+        return 7;
+    }
+}
+class AXIS1 implements Bank1 {
+    @Override
+    public int rateOfInterest() {
+        return 9;
+    }
+}
 public class Practice {
     public static void main(String[] args) {
-
+        SBI1 sbi = new SBI1();
+        ICICI1 icici = new ICICI1();
+        AXIS1 axis = new AXIS1();
+        System.out.println("Rate of Interest in SBI : " + sbi.rateOfInterest() + "%");
+        System.out.println("Rate of Interest in ICICI : " + icici.rateOfInterest() + "%");
+        System.out.println("Rate of Interest in AXIS : " + axis.rateOfInterest() + "%");
     }
 }
