7e0c3a6a1e9a8b188b5c3bd722c6eb387b98de86
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-28 22:09:22 +0530

    Practice

--- JAVA/Java_10_Inheritance_OverRiding/Practice.java
+++ JAVA/Java_10_Inheritance_OverRiding/Practice.java
@@ -1,4 +1,8 @@
-
+/* ------- A real example of Java Method Overriding --------
+   Consider a scenario where Bank is a class that provides functionality to get the rate of interest.
+   However, the rate of interest varies according to banks. For example, SBI, ICICI and AXIS banks
+   could provide 8%, 7%, and 9% rate of interest.
+*/
 
 package Java_10_Inheritance_OverRiding;
 
