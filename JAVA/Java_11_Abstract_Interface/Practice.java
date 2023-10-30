2ad4d7b49cd7cf2d0eff74751257f8b954c1cd10
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-30 21:00:27 +0530

    Practice

--- JAVA/Java_11_Abstract_Interface/Practice.java
+++ JAVA/Java_11_Abstract_Interface/Practice.java
@@ -1,35 +1,7 @@
-/*   Create a class monkey with jump ( ) and bite ( ) methods Create a class human which inherits
-     this monkey class and implements BasicAnimal interface with eat ( ) and sleep ( ) methods
- */
+// Create an abstract class pen with methods write () and refill () as abstract methods
 
 package Java_11_Abstract_Interface;
-class monkey {
-    void jump() {
-        System.out.println("Jumping");
-    }
-    void bite() {
-        System.out.println("Biting");
-    }
-}
 
-interface BasicAnimal1 {
-    void eat();
-    void sleep();
-}
-class Human1 extends monkey implements BasicAnimal1 {
-    public void eat() {
-        System.out.println("Eating");
-    }
-    public void sleep() {
-        System.out.println("Sleeping");
-    }
-}
 public class Practice {
-    public static void main(String[] args) {
-        Human1 human = new Human1();
-        human.eat();
-        human.sleep();
-        human.jump();
-        human.bite();
-    }
+
 }
