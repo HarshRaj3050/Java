81a72075ec8f741139fda403f82fe07706bba5bd
HarshRaj3050 <harshraj522004@gmail.com>
2023-10-30 21:03:46 +0530

    Practice

--- JAVA/Java_11_Abstract_Interface/Practice.java
+++ JAVA/Java_11_Abstract_Interface/Practice.java
@@ -1,7 +1,18 @@
 // Create an abstract class pen with methods write () and refill () as abstract methods
 
 package Java_11_Abstract_Interface;
-
+abstract class pen {
+    abstract void write();
+    abstract void refill();
+}
+class copy extends pen {
+    void write() {
+        System.out.println("Writing");
+    }
+    void refill() {
+        System.out.println("Refilling");
+    }
+}
 public class Practice {
 
 }
