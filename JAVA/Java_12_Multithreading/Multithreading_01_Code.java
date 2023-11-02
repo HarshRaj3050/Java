e846f6f003460e063c801fbee0c4a9da134fd3a5
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-02 20:48:57 +0530

    new question

---
+++ JAVA/Java_12_Multithreading/Multithreading_01_Code.java
@@ -0,0 +1,16 @@
+// Java Thread Example by extending Thread class
+
+package Java_12_Multithreading;
+class Multi extends Thread {
+    @Override
+    public void run() {
+        System.out.println("thread is running...");
+    }
+}
+public class Multithreading_01_Code {
+    public static void main(String[] args) {
+        Multi obj = new Multi();
+        obj.start();
+    }
+}
+6
