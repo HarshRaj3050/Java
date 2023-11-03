6f5157836454c9987d8fc24ae7f91d2fc1bbe1c3
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-03 16:20:35 +0530

    new question

--- JAVA/Java_13_Multithreading/Multithreading_03_Code.java
+++ JAVA/Java_13_Multithreading/Multithreading_03_Code.java
@@ -1,4 +1,20 @@
-package Java_13_Multithreading;
+// Write a code to Using Runnable interface
 
+package Java_13_Multithreading;
+class X implements Runnable {                                          // Step 1
+    @Override
+    public void run(){                                                 // Step 2
+        for(int i=1; i<=5; i++) {
+            System.out.println("\tThread X : " + i);
+        }
+        System.out.println("End of Thread X");
+    }
+}
 public class Multithreading_03_Code {
+    public static void main(String[] args) {
+        X runnable = new X();                                          // Step 3
+        Thread threadX = new Thread(runnable);                         // Step 4
+        threadX.start();                                               // Step 5
+        System.out.println("End of main Thread");
+    }
 }
