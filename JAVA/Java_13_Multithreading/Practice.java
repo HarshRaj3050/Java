ffcde0b5700f4f8651aa26f691602d7854b2f634
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-06 22:57:03 +0530

    Practice

--- JAVA/Java_13_Multithreading/Practice.java
+++ JAVA/Java_13_Multithreading/Practice.java
@@ -1,4 +1,20 @@
 package Java_13_Multithreading;
-
+class th2 extends Thread {
+    public void run() {
+        System.out.println("Hello");
+        System.out.println("I am Harsh");
+    }
+}
 public class Practice {
+    public static void main(String[] args) {
+        th2 t1 = new th2();
+        th2 t2 = new th2();
+        t1.start();
+        try {
+            t1.join();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        t2.start();
+    }
 }
