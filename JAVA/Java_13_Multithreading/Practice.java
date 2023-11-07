368062bd63bd76edc1866c645a13c9a293c6d7e3
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-07 22:47:45 +0530

    Practice

--- JAVA/Java_13_Multithreading/Practice.java
+++ JAVA/Java_13_Multithreading/Practice.java
@@ -1,20 +1,20 @@
+// Example of the sleep() Method in Java : on the main thread
+
 package Java_13_Multithreading;
-class th2 extends Thread {
+class Sleep extends Thread {
     public void run() {
-        System.out.println("Hello");
-        System.out.println("I am Harsh");
+        for(int i=1; i<=5; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println(i);
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 }
 public class Practice {
     public static void main(String[] args) {
-        th2 t1 = new th2();
-        th2 t2 = new th2();
-        t1.start();
-        try {
-            t1.join();
-        }catch (Exception e){
-            System.out.println(e);
-        }
-        t2.start();
+        
     }
 }
