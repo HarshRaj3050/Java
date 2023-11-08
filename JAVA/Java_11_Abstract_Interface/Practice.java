bcf095ea050ffbfafb5e3763b9cab2944925bfff
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-08 23:54:16 +0530

    Practice

--- JAVA/Java_11_Abstract_Interface/Practice.java
+++ JAVA/Java_11_Abstract_Interface/Practice.java
@@ -3,6 +3,25 @@
  */
 
 package Java_11_Abstract_Interface;
-
+class monkey {
+    public void jump() {
+        System.out.println("Jumping");
+    }
+    public void bite() {
+        System.out.println("Biting");
+    }
+}
+interface BasicAnimal45 {
+    void eat();
+    void sleep();
+}
+class Human45 extends monkey implements BasicAnimal45{
+    public void eat() {
+        System.out.println("Eating");
+    }
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+}
 public class Practice {
 }
