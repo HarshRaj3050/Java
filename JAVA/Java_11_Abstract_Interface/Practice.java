aa43b1747bf63cd269d56c3f2a36cd7a42102b68
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-08 23:42:49 +0530

    Practice

--- JAVA/Java_11_Abstract_Interface/Practice.java
+++ JAVA/Java_11_Abstract_Interface/Practice.java
@@ -1,20 +1,8 @@
-// Create an abstract class pen with methods write () and refill () as abstract methods
+/*   Create a class monkey with jump ( ) and bite ( ) methods Create a class human which inherits
+     this monkey class and implements BasicAnimal interface with eat ( ) and sleep ( ) methods
+ */
 
 package Java_11_Abstract_Interface;
-abstract class pen {
-    abstract void write();
-    abstract void refill();
-}
-class copy extends pen {
-    void write() {
-        System.out.println("Writing");
-    }
-    void refill() {
-        System.out.println("Refilling");
-    }
-}
+
 public class Practice {
-    public static void main(String[] args) {
-        copy copy = new copy();
-    }
 }
