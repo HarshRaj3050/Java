b045599682237e1c31f2a8e6637eac4127d7870e
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-09 22:47:23 +0530

    Practice

--- JAVA/Java_04_Condition/Practice.java
+++ JAVA/Java_04_Condition/Practice.java
@@ -1,8 +1,25 @@
 // Write Java Program to Find Greatest of Two Numbers
 
 package Java_04_Condition;
+import java.util.Scanner;
 public class Practice{
+    public static void greatestNum(int num1, int num2) {
+        if(num1 == num2) {
+            System.out.println("Both Number is Same");
+        }
+        else if (num1 > num2) {
+            System.out.println("First Number is Greatest");
+        }
+        else {
+            System.out.println("Second Number is Greatest");
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int num1 = scan.nextInt();
+        System.out.print("Enter Second Number : ");
+        int num2 = scan.nextInt();
+        greatestNum(num1, num2);
     }
 }
