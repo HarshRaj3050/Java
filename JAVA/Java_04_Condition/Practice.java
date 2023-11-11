9af3ffff1e9aebb96afb293254d4f3304d0da509
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-11 22:19:14 +0530

    Practice

--- JAVA/Java_04_Condition/Practice.java
+++ JAVA/Java_04_Condition/Practice.java
@@ -4,6 +4,18 @@ package Java_04_Condition;
 import java.util.Scanner;
 public class Practice{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Year : ");
+        int year = scan.nextInt();
+
+        if(year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " is leap Year");
+        }
+        else if (year % 400 == 0){
+            System.out.println(year + " is leap Year");
+        }
+        else {
+            System.out.println(year + " is Not leap Year");
+        }
     }
 }
