20782a7bacd0842bc7f5e3efce49e946adafc03d
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-12 22:14:11 +0530

    Practice

--- JAVA/Java_04_Condition/Practice.java
+++ JAVA/Java_04_Condition/Practice.java
@@ -1,21 +1,37 @@
-// Write a Java program to find whether a year entered by the user is a leap year or not.
+/*  Write a Java program to find out the day of the week given the number
+    [1 for Monday, 2 for Tuesday … and so on!]  */
 
 package Java_04_Condition;
 import java.util.Scanner;
 public class Practice{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the Year : ");
-        int year = scan.nextInt();
-
-        if(year % 4 == 0 && year % 100 != 0) {
-            System.out.println(year + " is leap Year");
-        }
-        else if (year % 400 == 0){
-            System.out.println(year + " is leap Year");
+        System.out.println("1 for Monday\n2 for Tuesday\n3 for Wednesday\n4 for Thursday\n" +
+                "5 for Friday\n6 for Saturday\n7 for Sunday");
+        System.out.print("----Enter the Number : ");
+        int num = scan.nextInt();
+        if(num>=1 && num<=7) {
+            if(num==1) {
+                System.out.println("Day is : Monday");
+            }
+            else if(num==2) {
+                System.out.println("Day is : Tuesday");
+            }
+            else if(num==3) {
+                System.out.println("Day is : Wednesday");
+            }
+            else if(num==4) {
+                System.out.println("Day is : Thursday");
+            }
+            else if(num==5) {
+                System.out.println("Day is : Friday");
+            }
+            else if(num==6) {
+                System.out.println("Day is : Saturday");
+            }
         }
         else {
-            System.out.println(year + " is Not leap Year");
+            System.out.println("Enter the valid Number");
         }
     }
 }
