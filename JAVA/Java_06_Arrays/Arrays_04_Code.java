9b6debdccda24b93b4a8a6396e57837a2c311fb9
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-13 22:04:39 +0530

    Practice

---
+++ JAVA/Java_06_Arrays/Arrays_04_Code.java
@@ -0,0 +1,11 @@
+// Creating a method which receives an array as a parameter
+
+package Java_06_Arrays;
+public class Arrays_04_Code {
+    
+    public static void main(String[] args) {
+        int a [] = {33,3,5,6,};
+        // passing array to method
+        min(a);
+    }
+}
