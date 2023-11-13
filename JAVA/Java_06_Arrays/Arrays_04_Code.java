c9e040b2a38a8a197e857adbc9e3d8c76530f8a9
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-13 22:07:01 +0530

    Code

--- JAVA/Java_06_Arrays/Arrays_04_Code.java
+++ JAVA/Java_06_Arrays/Arrays_04_Code.java
@@ -2,9 +2,17 @@
 
 package Java_06_Arrays;
 public class Arrays_04_Code {
-    
+    public static void min(int arr[]) {
+        int min = arr[0];
+        for(int i=0; i<arr.length; i++) {
+            if(min<arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
     public static void main(String[] args) {
-        int a [] = {33,3,5,6,};
+        int a [] = {33,3,5,6};
         // passing array to method
         min(a);
     }
