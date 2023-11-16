9882ea4d20ac6a378f8e18bff43427457145d2fe
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-16 21:34:18 +0530

    Practice

--- JAVA/Java_06_Arrays/practice.java
+++ JAVA/Java_06_Arrays/practice.java
@@ -4,16 +4,16 @@ package Java_06_Arrays;
 import java.util.Scanner;
 public class practice {
     static void sorted(int arr[]) {
-        int num = arr[0];
+        int prevNum = arr[0];
+        boolean check = true;
         for(int i=0; i<arr.length; i++) {
-            if(num > arr[i]) {
-                System.out.println("Array is not sorted");
-                break;
-            }
-            else if(i+1 ==  arr.length) {
-                System.out.println("Array is sorted");
-            }
-            num = arr[i];
+           if(arr[i] < prevNum) {
+               check = false;
+               break;
+           }
+           else {
+               prevNum = arr[i];
+           }
         }
     }
     public static void main(String[] args) {
