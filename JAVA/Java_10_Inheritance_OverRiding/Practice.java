09e7c0f1fbf075716318924e401b7725f87355fc
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-17 21:31:03 +0530

    Practice

--- JAVA/Java_10_Inheritance_OverRiding/Practice.java
+++ JAVA/Java_10_Inheritance_OverRiding/Practice.java
@@ -1,31 +1,27 @@
-/* Create a class circle and use inheritance to create another class cylinder and find the
-   Area of Circle and Volume of Cylinder, user can Enter the Radius and Height.
- */
+// Create a class Animal and Derive another class dog from it. and Enter Name and Age
 
 package Java_10_Inheritance_OverRiding;
-class Circle1 {
-    public int radius;
-    public Circle1(int radius) {
-        this.radius = radius;
+class Animal2 {
+    int age;
+    String name;
+    Animal2(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
-    public void areaCircle() {
-        System.out.printf("Area of Circle is : %.2f\n", 3.14f * (radius * radius));
+    void name (String name) {
+        System.out.println("Name is : " + name);
+    }
+    void age (int age) {
+        System.out.println("Age is : " + age);
     }
 }
-class Cylinder1 extends Circle1 {
-    int height;
-    public Cylinder1(int radius, int height) {
-        super(radius);
-        this.height = height;
-    }
-    public void areaCylinder() {
-        System.out.printf("Volume of Cylinder is : %.2f\n", 3.14f * (radius*radius) * height);
+class Dog2 extends Animal2 {
+    Dog2(String name, int age) {
+        super(name,age);
     }
 }
 public class Practice {
-    public static void main(String[] args) {
-        Cylinder1 cylinder = new Cylinder1(5,3);
-        cylinder.areaCircle();
-        cylinder.areaCylinder();
+    public static void main(String [] args) {
+
     }
 }
