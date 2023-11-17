1ad37645984175e58d66670af651cfd626ddd921
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-17 22:32:56 +0530

    Practice

--- JAVA/Java_10_Inheritance_OverRiding/Practice.java
+++ JAVA/Java_10_Inheritance_OverRiding/Practice.java
@@ -1,6 +1,7 @@
 // Create a class Animal and Derive another class dog from it. and Enter Name and Age
 
 package Java_10_Inheritance_OverRiding;
+import java.util.Scanner;
 class Animal2 {
     int age;
     String name;
@@ -8,10 +9,8 @@ class Animal2 {
         this.name = name;
         this.age = age;
     }
-    void name (String name) {
+    void details () {
         System.out.println("Name is : " + name);
-    }
-    void age (int age) {
         System.out.println("Age is : " + age);
     }
 }
@@ -22,6 +21,12 @@ class Dog2 extends Animal2 {
 }
 public class Practice {
     public static void main(String [] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Name : ");
+        String name = scan.nextLine();
+        System.out.print("Enter Age : ");
+        int age = scan.nextInt();
+        Dog2 dog = new Dog2(name, age);
+        dog.details();
     }
 }
