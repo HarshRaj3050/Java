6dd89330514238d14b584cab454e2b5de5b655b0
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-28 14:36:51 +0530

    Practice

--- JAVA/Java_01_Basic/Basic_10_Question.java
+++ JAVA/Java_01_Basic/Basic_10_Question.java
@@ -1,23 +1,12 @@
-// Write a program to calculate CGPA using marks of three subjects (out of 100)
+// Write a Java program to detect whether a number entered by the user is an integer or not
 
 package Java_01_Basic;
 import java.util.Scanner;
 public class Basic_10_Question {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("------- Enter Three subject Marks -------");
-
-        System.out.print("Enter First subject number : ");
-        float sub1 = scan.nextInt();
-        System.out.print("Enter Second subject number : ");
-        float sub2 = scan.nextInt();
-        System.out.print("Enter Third subject number : ");
-        float sub3 = scan.nextInt();
-
-        float allSub = sub1 + sub2 + sub3;
-        float cgpa = allSub / 30;
-        System.out.print("The CGPA of three subject is : ");
-        System.out.println(cgpa);
-
+        System.out.println("if number is integer return 'ture' else 'false'");
+        System.out.print("Enter the Number : ");
+        System.out.println(scan.hasNextInt());
     }
 }
--- JAVA/Java_01_Basic/Basic_Practice.java
+++ JAVA/Java_01_Basic/Basic_Practice.java
@@ -1,15 +1,18 @@
-// Write a JAVA code Add any two number. ( User can inter number )
+// Write a program to calculate CGPA using marks of three subjects (out of 100)
 
 package Java_01_Basic;
 import java.util.Scanner;
 class Basic_Practice {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter First Number : ");
-        int first = scan.nextInt();
-        System.out.print("Enter Second Number : ");
-        int second = scan.nextInt();
-        int result = first + second;
-        System.out.println("Sum is : " + result);
+        System.out.print("Enter First subject Marks : ");
+        float sub1 = scan.nextInt();
+        System.out.print("Enter Second subject Marks : ");
+        float sub2 = scan.nextInt();
+        System.out.print("Enter Third subject Marks : ");
+        float sub3 = scan.nextInt();
+
+        float CGPA = (sub1 + sub2 + sub3) / 30.f;
+        System.out.println("CGPA : " + CGPA);
     }
 }
