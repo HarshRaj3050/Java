635be733a9ece6c587f53d91e7ded54e7298524e
HarshRaj3050 <harshraj522004@gmail.com>
2023-11-28 14:22:47 +0530

    Practice

--- JAVA/Java_01_Basic/Basic_Practice.java
+++ JAVA/Java_01_Basic/Basic_Practice.java
@@ -1,15 +1,15 @@
-// Write a Java program to convert Kilometers to miles.
+// Write a JAVA code Add any two number. ( User can inter number )
 
 package Java_01_Basic;
 import java.util.Scanner;
 class Basic_Practice {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("-----KILOMETERS  TO  MILES-----");
-        System.out.print("Enter the Kilometers : ");
-        double kilo = scan.nextFloat();
-        double mile = kilo * 0.621371;
-        System.out.println(kilo + " Kilometers is : " + mile + " Miles");
+        System.out.print("Enter First Number : ");
+        int first = scan.nextInt();
+        System.out.print("Enter Second Number : ");
+        int second = scan.nextInt();
+        int result = first + second;
+        System.out.println("Sum is : " + result);
     }
 }
