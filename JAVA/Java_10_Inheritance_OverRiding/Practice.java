66d29060afbbdf22a289cc5c0fb0af041365ab7d
HarshRaj3050 <harshraj522004@gmail.com>
2023-12-03 19:34:22 +0530

    Practice

--- JAVA/Java_10_Inheritance_OverRiding/Practice.java
+++ JAVA/Java_10_Inheritance_OverRiding/Practice.java
@@ -1,6 +1,23 @@
 //Java Program to illustrate the use of Java Method Overriding
 
 package Java_10_Inheritance_OverRiding;
-import java.util.Scanner;
+class Amit {
+    void writing() {
+        System.out.println("java");
+    }
+}
+class Amit5 extends Amit {
+    @Override
+    void writing() {
+        System.out.println("Python");
+    }
+}
 public class Practice {
+
+    public static void main(String[] args) {
+        Amit ab = new Amit();
+        Amit5 a = new Amit5();
+        ab.writing();
+        a.writing();
+    }
 }
