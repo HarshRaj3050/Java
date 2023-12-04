e2310599b46ed2724b99fd63e99b21ec18ac6b0f
HarshRaj3050 <harshraj522004@gmail.com>
2023-12-04 12:24:21 +0530

    Practice

--- JAVA/Java_10_Inheritance_OverRiding/Practice.java
+++ JAVA/Java_10_Inheritance_OverRiding/Practice.java
@@ -1,6 +1,7 @@
 //Java Program to illustrate the use of Java Method Overriding
 
 package Java_10_Inheritance_OverRiding;
+import java.util.Scanner;
 class Super1 {
     String name;
     Super1(String name) {
@@ -9,9 +10,15 @@ class Super1 {
     }
 }
 class Sub1 extends Super1 {
-    Sub1 {
+    Sub1(String name) {
         super(name);
     }
 }
 public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter name : ");
+        String name = scan.nextLine();
+        Sub1 sub = new Sub1(name);
+    }
 }
