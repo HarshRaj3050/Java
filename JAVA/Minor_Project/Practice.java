b4a3ddb2f98b23970c82d3cf5ff8a24261bb70f8
HarshRaj3050 <harshraj522004@gmail.com>
2023-12-04 12:44:08 +0530

    Practice

---
+++ JAVA/Minor_Project/Practice.java
@@ -0,0 +1,15 @@
+// Create a class Game, which allows a user to play "Guess the Number" game once.
+
+package Minor_Project;
+import java.util.Scanner;
+import java.util.Random;
+
+class Guess {
+}
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        
+    }
+}
