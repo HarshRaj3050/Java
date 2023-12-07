271a309cf24a8f8427da568d02ad2cbb4e0032d3
HarshRaj3050 <harshraj522004@gmail.com>
2023-12-07 19:52:41 +0530

    Practice

--- JAVA/Java_11_Abstract_Interface/Practice.java
+++ JAVA/Java_11_Abstract_Interface/Practice.java
@@ -1,22 +1,49 @@
-// Create an abstract class pen with methods write () and refill () as abstract methods
+// Example of an Interface, the Drawable interface has only one method.
+// Its implementation is provided by Rectangle and Circle classes.
 
 package Java_11_Abstract_Interface;
-abstract class Pen1 {
-    abstract void write();
-    abstract void refill();
+import java.util.Scanner;
+interface Area {
+    void area();
 }
-class BallPen1 extends Pen {
-    void write() {
-        System.out.println("Hello World");
+class Rectangle2 implements Area {
+    int l,b;
+    Rectangle2 (int l, int b) {
+        this.l = l;
+        this.b = b;
     }
-    void refill() {
-        System.out.println("This pen is black");
+    public void area() {
+        int area;
+        area = l*b;
+        System.out.println("Area of Rectangle : " + area);
+    }
+}
+class Circle2 implements Area {
+    int r;
+    Circle2(int r) {
+        this.r = r;
+    }
+    public void area() {
+        float areaOfCircle;
+        areaOfCircle = 3.14f * (r * r);
+        System.out.println("Area of Circle : " + areaOfCircle);
     }
 }
 public class Practice {
-    public static void main(String[] args) {
-        BallPen1 ballPen = new BallPen1();
-        ballPen.write(); // for print text
-        ballPen.refill(); // 
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the L : ");
+        int l = scan.nextInt();
+        System.out.print("Enter the B : ");
+        int b = scan.nextInt();
+        System.out.print("Enter the Radius : ");
+        int r = scan.nextInt();
+        Rectangle2 rect = new Rectangle2(l,b);
+        Circle2 circle = new Circle2(r);
+        rect.area();
+        circle.area();
+
+
+
     }
 }
