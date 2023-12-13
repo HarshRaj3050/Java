3cc030745d8e464a66892ef83b58ebe8500398c2
HarshRaj3050 <harshraj522004@gmail.com>
2023-12-14 00:26:12 +0530

    update

--- JAVA/Java_01_Basic/Basic_16_Question.java
+++ JAVA/Java_01_Basic/Basic_16_Question.java
@@ -7,9 +7,13 @@ public class Basic_16_Question {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the Number : ");
         int num = scan.nextInt();
+        System.out.print("Factor of " + num + " is : ");
         for(int i=1; i<=num; i++) {
             if(num % i == 0) {
-                System.out.println(i);
+                System.out.print(i);
+                if(i!=num) {
+                    System.out.print(", ");
+                }
             }
         }
     }
