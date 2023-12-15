06e5c3f6443f561bfe756a07db5d912253cec873
HarshRaj3050 <harshraj522004@gmail.com>
2023-12-15 21:13:02 +0530

    new question

--- JAVA/Java_05_Loops/Star_Question_Loop/Star_03_Question.java
+++ JAVA/Java_05_Loops/Star_Question_Loop/Star_03_Question.java
@@ -1,6 +1,25 @@
-/* Write a program to print the following pattern : */
+/* Write a program to print the following pattern :
+        A
+        BA
+        CBA
+        DCBA
+        EDCBA                                     */
 
 package Java_05_Loops.Star_Question_Loop;
 
 public class Star_03_Question {
+    public static void main(String[] args) {
+        int i=2;
+        int j=65;
+        int k=65;
+        while(i<=6) {
+            while(j<=k) {
+                System.out.printf("%c", k);
+                k--;
+            }
+            k = k+i;
+            System.out.println();
+            i++;
+        }
+    }
 }
--- JAVA/Java_05_Loops/Star_Question_Loop/Star_04_Question.java
+++ JAVA/Java_05_Loops/Star_Question_Loop/Star_04_Question.java
@@ -3,4 +3,8 @@
 package Java_05_Loops.Star_Question_Loop;
 
 public class Star_04_Question {
+    public static void main(String[] args) {
+        int i = 65;
+        System.out.printf("%c", i);
+    }
 }
